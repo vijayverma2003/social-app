84c2939e9129dae5ba58396aea668769517d1095
Vijay <vijayvermakvp@gmail.com>
2025-11-16 16:27:31 +0530

    merge existing web code into imported repo history

--- temp-web/app/not-found.tsx
+++
@@ -1,9 +0,0 @@
-const NotFound = () => {
-  return (
-    <div className="flex h-screen items-center justify-center">
-      <p className="text-sm font-bold">404 - Page Not Found</p>
-    </div>
-  );
-};
-
-export default NotFound;
