e2682baf64d109b018f1690c6fd54a6473e3448b
Vijay <vijayvermakvp@gmail.com>
2025-11-30 20:34:29 +0530

    Update pnpm-lock.yaml and pnpm-workspace.yaml to include new dependencies and restructure workspace. Added database package to workspace and updated various package versions, including Prisma and PostgreSQL related packages, enhancing compatibility and functionality.

---
+++ database/src/client.ts
@@ -0,0 +1,21 @@
+import { PrismaClient } from "./generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { Pool } from "pg";
+
+// PrismaClient is attached to the `global` object in development to prevent
+// exhausting your database connection limit.
+// Learn more: https://pris.ly/d/help/next-js-best-practices
+
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+const adapter = new PrismaPg(pool);
+
+export const prisma = globalForPrisma.prisma ?? new PrismaClient({ adapter });
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
