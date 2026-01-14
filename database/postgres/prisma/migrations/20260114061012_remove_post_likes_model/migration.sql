41c773dce48d0fd4cfaf5c0327cbe002724ab662
Vijay <vijayvermakvp@gmail.com>
2026-01-14 11:41:09 +0530

    Refactor Post Model and Remove PostLikes
    - Removed the PostLikes model and its associated references from the User and Post models.
    - Updated the Post model to include a likes count as an integer with a default value of 0, simplifying the like functionality.
    - This change streamlines the schema and improves data management for post interactions.

---
+++ database/postgres/prisma/migrations/20260114061012_remove_post_likes_model/migration.sql
@@ -0,0 +1,14 @@
+/*
+  Warnings:
+
+  - You are about to drop the `PostLikes` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE "PostLikes" DROP CONSTRAINT "PostLikes_postId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "PostLikes" DROP CONSTRAINT "PostLikes_userId_fkey";
+
+-- DropTable
+DROP TABLE "PostLikes";
