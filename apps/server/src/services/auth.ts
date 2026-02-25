648b9b3ad800206e56af286fb7b74ce2d0d7a337
Vijay <vijayvermakvp@gmail.com>
2026-02-25 11:41:25 +0530

    Update pnpm-lock and package.json for better-auth integration and enhance Prisma schema
    - Added better-auth package version 1.4.19 to package.json for authentication features.
    - Updated pnpm-lock.yaml to include dependencies for better-auth and related packages.
    - Enhanced Prisma schema by adding User, Session, Account, and Verification models to support user authentication and session management.

---
+++ apps/server/src/services/auth.ts
@@ -0,0 +1,7 @@
+import prisma from "@database/postgres";
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+
+export const auth = betterAuth({
+  database: prismaAdapter(prisma, { provider: "postgresql" }),
+});
